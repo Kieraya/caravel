5a28b4b97c6d7a366c75c7e223a6a7415460d3f9
naveen yannamani <naveen.yannamani@furlenco.com>
2016-11-02 17:50:50 +0530

    https redirect

--- caravel/views.py
+++ caravel/views.py
@@ -2341,6 +2341,13 @@ appbuilder.add_link(
     category_icon="fa-flask",
     category='SQL Lab')
 
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
 
 @app.after_request
 def apply_caching(response):
